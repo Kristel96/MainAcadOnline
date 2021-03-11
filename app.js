3398076b7f0164edd395d8facb7410ea3a36513b
Khrystyna Rym <rimar1996@gmail.com>
2021-03-11 16:10:24 +0200

    all examples

--- app.js
+++ app.js
@@ -1,55 +1,130 @@
-// Создайте массив с названиями дней недели. Вывести сегодняшний день недели словами.
 
-let weekDays =["Sunday","Monday","Tuesday", "Wednesday","Thursday","Friday","Saturday"];
-let today = new Date();
- // getDay();
-let dayNumber = today.getDay();
-console.log(dayNumber);
-console.log(weekDays[dayNumber]);
+setInterval(function (){
+    console.log("hello there")
+
+},1000 );// не менше ніж через
 
 
-//JSON methods
+
 
 let user = {
- name: "John",
- lastName: "Gold",
- phone: 12121212,
- emails:["qwqw@gmail.com",
- "jfjfj@gmail.com"]
+    name: "Tom",
+    email:"qwerty@gmail.com",
+    display: function (){
+      console.log(user.name) ;
+      console.log(user.email) ;
+    },
+    sayHI:()=>{
+        alert("Hello");
+    },
+    changeName(){
+        user.name = "John";
+        console.log( user.name);
+    },
+    education:{
+        school:"State Schol 11",
+        university: "Technical",
+        degree: "master of Computer Sciemce"
+    },
+    children:[
+            { name: "Daisy", age:10 },
+            { name: "Lola", age:5 },
+            { name: "Max", age:13 }
+    ]
+};
+
+
+let hasNameProp = user.hasOwnProperty('email');
+let hasNameProp2 = user.hasOwnProperty('lastName');
+
+
+console.log(hasNameProp);
+console.log(hasNameProp2);
+
+
+let user = {};
+user.name = "Tom";
+user.age = 26;
+user.display = function(){
+
+    console.log(user.name);
+    console.log(user.age);
+};
+for(let key in user) {
+    console.log(key + " : " + user[key]);
 }
 
 
-let jsonObj = JSON.stringify(user);
-console.log(jsonObj);
-console.log(jsonObj.lastName)
+function createUser(pName, pAge) {
+    return {
+        name: pName,
+        age: pAge,
+        displayInfo: function() {
+            document.write("Имя: " + this.name + " возраст: " + this.age + "<br/>");
+        },
+        driveCar: function(car){
+            document.write(this.name + " ведет машину " + car + "<br/>");
+        }
+    };
+};
 
-let standartObject = JSON.parse(jsonObj);
-console.log(standartObject);
+function createCar(mName, mYear){
+    return{
+        name: mName,
+        year: mYear
+    };
+}
+let tom = createUser("Том", 26);
+tom.displayInfo();
 
-console.log(standartObject.name);
+
+// let bently = createCar("Бентли", 2004);
+tom.driveCar("Opel");
+
+ let tom = new Object();
 
 
 
-///FUNCTIONS
 
-
-//Standart function view
-main();
-
-function main(){
- console.log("Hello")
+function User(pName, pAge) {
+    this.name = pName;
+    this.age = pAge;
+    this.driveCar = function(car){
+        document.write(this.name + " ведет машину " + car.name + "<br/>");
+    };
+    this.displayInfo = function(){
+        document.write("Имя: " + this.name + "; возраст: " + this.age + "<br/>");
+    };
 }
 
-//Anonymous function
-let a = function(){
- console.log("Hello")
+////Інкапсуляція
+function User (name) {
+    this.name = name;
+    let _age = 1;
+    this.displayInfo = function(){
+        console.log("Имя: " + this.name + "; возраст: " + _age);
+    };
+    this.getAge = function() {
+        return _age;
+    }
+    this.setAge = function(age) {
+        if(typeof age === "number" && age >0 && age<110){
+            _age = age;
+        } else {
+            console.log("Недопустимое значение");
+        }
+    }
 }
-a();
 
-//Arrow function
-let b =()=>{
- console.log("Hello")
+let tom = new User("Том");
+console.log(tom._age); // undefined - _age - локальная переменная
+console.log(tom.getAge()); // 1
+tom.setAge(32);
+console.log(tom.getAge()); // 32
+tom.setAge("54"); // Недопустимое значение
+tom.setAge(123); // Недопустимое значение
+
+function Hello(){
+
 }
-b();
-
-
+///
