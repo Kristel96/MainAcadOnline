46cb6bff99a85655af239904d98a37696eedfe99
khrystyna <khrystyna.rymar@gmail.com>
2021-03-16 21:51:00 +0200

    home task+ Work with DOM

--- functions.js
+++ functions.js
@@ -1,40 +1,21 @@
-//Потрібно знайти периметр чотирикутника,площу та периметр прямокутника та квадрата
+let container = document.getElementById('myDiv');
+let imgTag = document.createElement("img");
+imgTag.src = "https://images.unsplash.com/photo-1541690161184-75cbc0386cd6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80";
+imgTag.style.width = "150px";
+container.appendChild(imgTag);
 
-let sidesArray=[2,3]
-// Rectangle Square Quadrangle
 
-function perimeter(sides, type){
-    let p;
-    switch (type){
-        case "Quadrangle":
-            p = sum(sides);
-            return p;
-        case "Square":
-            if(!Array.isArray(sides)){
-                p = sides*4;///лише якщо на вхід передамо одне число без масива
-                return p;
-            }
-            break;
-        case "Rectangle":
-            if (sides.length===2){
-                p = sum(sides);
-                return 2*p; ///якщо на вхід прийде два елементи
-            }
-            break;
-        default:
-            p = sum(sides);
-            return p;
-    }
+let header = document.createElement("h3");
+// let headerText = document.createTextNode("Історія про степлер");
+// // header.appendChild(headerText);                                             альтернатива
+header.innerText = "Історія про степлер";
+container.appendChild(header);
 
-}
-function sum(array){
-    let total= 0;
-    for (let j=0; j<array.length; j++){
-        total+=array[j];
-    }
-    return total;
-}
 
-console.log(perimeter(sidesArray, "Rectangle"));
-
-///д.З додати функції для знаходження площі
+let article = document.createElement("p");
+article.innerText = "Progressively create real-time initiatives whereas wireless alignments. Phosfluorescently aggregate best-of-breed process improvements through focused strategic theme areas. Proactively whiteboard value-added content after multimedia based niche markets. Uniquely e-enable customer directed web services whereas backend systems. Assertively grow client-centered paradigms before technically sound sources.\n" +
+    "\n" +
+    "Proactively fabricate future-proof alignments via distinctive vortals. Assertively productize exceptional \"outside the box\" thinking after highly efficient benefits. Compellingly incubate cross-media alignments and premier solutions. Credibly generate customer directed communities vis-a-vis alternative catalysts for change. Proactively supply turnkey methods of empowerment vis-a-vis granular markets.\n" +
+    "\n" +
+    "Dynamically expedite turnkey synergy before dynamic catalysts for change. Assertively scale team building relationships and distinctive services. Credibly strategize standardized deliverables and web-enabled growth strategies. Professionally empower cutting-edge action items with team building technologies. Synergistically incubate intuitive.";
+container.appendChild(article);
