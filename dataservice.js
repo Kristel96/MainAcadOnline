e9dca90939eb5a38c7226a8d66a6c0b45a2ba22a
Khrystyna Rym <rimar1996@gmail.com>
2021-04-20 21:31:31 +0300

    added a search film part

--- dataservice.js
+++ dataservice.js
@@ -1,7 +1,23 @@
+const urlBase = 'https://api.themoviedb.org/3';
+const urlMovie = '/movie/popular?api_key=';
+const apiKey = '3bcb757b3ebe4adeeb1c3a7d2a3c4dcc';
+const urlSearch = '/search/movie?api_key='
+const query = '&query=';
 
-export function getMovies(fullUrl,callback){
-    fetch(fullUrl)
+const fullgetMovieUrl = urlBase+urlMovie+apiKey;
+const seachMovieUrl = urlBase+urlSearch+apiKey+query;
+
+export function getMovies(callback){
+    fetch(fullgetMovieUrl)
         .then(result => result.json())
         .then(result =>  callback(result.results))
         .catch(error => alert(error));
 }
+export function searchMovie(movie, callback){
+    fetch(seachMovieUrl+movie)
+        .then(result => result.json())
+        .then(result =>  callback(result))
+}
+//
+// https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&query=dghjhfgj&page=1&include_adult=false
+// https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
--- films.js
+++ films.js
@@ -1,24 +1,70 @@
-import {getMovies} from './dataservice.js'
+import {getMovies, searchMovie} from './dataservice.js'
 
 let imgBaseServer = 'https://image.tmdb.org/t/p/w300/';
-let urlBase = 'https://api.themoviedb.org/3/movie/popular?api_key=';
-let apiKey = '3bcb757b3ebe4adeeb1c3a7d2a3c4dcc';
-
-
-const fullUrl = urlBase+apiKey;
 window.onload = function() {
-getMovies(fullUrl, createFilmsList);
-
+    getMovies(createFilmsList);
 }
+///ДЗ стоврити клас для фільмів і пацювати з екземпляром класу=>
+let container = document.getElementById('mainCont');
+
+
 function createFilmsList(filmArray){
     console.log(filmArray);
-    let container = document.getElementById('mainCont');
-    filmArray.forEach((item, index)=>{
+    filmArray.forEach((item, index)=> {
+    let leftBlock = document.createElement('div');
+        leftBlock.className = 'leftblock';
+    let rightBlock = document.createElement('div');
+        rightBlock.className = 'rightblock';
+
+
     let poster = document.createElement('img');
+    poster.className = 'filmPoster';
+
+    let title =   document.createElement('h3');
+    title.className = 'filmTitle';
+    title.innerText = item.original_title;
+
+    let desc = document.createElement('p');
+        desc.innerText = item.overview;
+        rightBlock.append(desc);
+
     let filmWrapper = document.createElement('div');
     filmWrapper.className = "film-wrapper";
     poster.src = imgBaseServer+item.poster_path;
-    filmWrapper.append(poster);
+
+        leftBlock.append(poster);
+        leftBlock.append(title);
+
+        filmWrapper.append(leftBlock);
+        filmWrapper.append(rightBlock);
+
     container.append(filmWrapper);
     })
 }
+
+document.getElementById('searchFilm').onclick = function (){
+   let searchQuery = document.getElementById('search').value;
+    searchMovie(searchQuery,showSearch);
+}
+
+
+
+function showSearch(response){
+    container.innerHTML = '';
+    createFilmsList(response.results)
+}
+
+// adult: false
+// backdrop_path: "/inJjDhCjfhh3RtrJWBmmDqeuSYC.jpg"
+// genre_ids: (2) [28, 878]
+// id: 399566
+// original_language: "en"
+// original_title: "Godzilla vs. Kong"
+// overview: "In a time when monsters walk the Earth, humanity’s fight for its future sets Godzilla and Kong on a collision course that will see the two most powerful forces of nature on the planet collide in a spectacular battle for the ages."
+// popularity: 6159.271
+// poster_path: "/pgqgaUx1cJb5oZQQ5v0tNARCeBp.jpg"
+// release_date: "2021-03-24"
+// title: "Godzilla vs. Kong"
+// video: false
+// vote_average: 8.3
+// vote_count: 4676
