b17281f8601ae26833849d4552a1483044517019
khrystyna <khrystyna.rymar@gmail.com>
2021-04-29 21:56:43 +0300

    session is authentificaton

--- dataservice.js
+++ dataservice.js
@@ -1,11 +1,24 @@
 const urlBase = 'https://api.themoviedb.org/3';
 const urlMovie = '/movie/popular?api_key=';
 const apiKey = '3bcb757b3ebe4adeeb1c3a7d2a3c4dcc';
-const urlSearch = '/search/movie?api_key='
+const accesToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYmNiNzU3YjNlYmU0YWRlZWIxYzNhN2QyYTNjNGRjYyIsInN1YiI6IjVkMzk5OWRhYWI2ODQ5MTA1YjhhOTZiNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FRBWe0HGh-bOSXRKJy0FOe4rjW8G-bIgPxFWLQq3q1s';
+
+const urlSearch = '/search/movie?api_key=';
+const urlRequestToken = '/authentication/token/new?api_key=';
+const urlSession = '/authentication/token/validate_with_login?api_key=';
 const query = '&query=';
 
 const fullgetMovieUrl = urlBase+urlMovie+apiKey;
 const seachMovieUrl = urlBase+urlSearch+apiKey+query;
+const requestTokenUrl = urlBase+urlRequestToken+ apiKey;
+const sesionIdTokenUrl = urlBase+urlSession+apiKey;
+
+
+// https://api.themoviedb.org/3/authentication/token/new?api_key=<<api_key>>
+//https://api.themoviedb.org/3/authentication/token/validate_with_login?api_key=<<api_key>>
+
+
+//https://api.themoviedb.org/3/authentication/session/new?api_key=<<api_key>>
 
 export function getMovies(callback){
     fetch(fullgetMovieUrl)
@@ -17,6 +30,58 @@ export function searchMovie(movie, callback){
     fetch(seachMovieUrl+movie)
         .then(result => result.json())
         .then(result =>  callback(result))
+}
+
+export function getRequestToken(uname, psw, callback){
+    fetch(requestTokenUrl)
+        .then(result=>result.json())
+        .then(result=> callback(uname, psw, result.request_token));
+}
+export function validateWithLogin(username, pasw, reqToken){
+    let req = {
+        username:username,
+        password:pasw,
+        request_token: reqToken
+    };
+    let response ={};
+    fetch(sesionIdTokenUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(req)
+    })
+        .then(result=>result.json())
+        .then(result=> createSession(result.request_token));
+
+}
+ function createSession(requestToken){
+    let req ={
+        request_token: requestToken
+    };
+
+     fetch('https://api.themoviedb.org/3/authentication/session/new?api_key=3bcb757b3ebe4adeeb1c3a7d2a3c4dcc', {
+         method: 'POST',
+         headers: {
+             'Content-Type': 'application/json'
+         },
+         body: JSON.stringify(req)
+     }).then(result=>result.json())
+         .then(result=> console.log(result));
+}
+export function getAccess(){
+   let req =  {
+        access_token: accesToken,
+    };
+    fetch('https://api.themoviedb.org/3/authentication/session/convert/4?api_key=3bcb757b3ebe4adeeb1c3a7d2a3c4dcc', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(req)
+    })
+        .then(result=>result.json())
+        .then(result=> console.log(result));
 }
 //
 // https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&query=dghjhfgj&page=1&include_adult=false
--- login.js
+++ login.js
@@ -1,10 +1,18 @@
-let form = document.forms.loginForm;
-form.onsubmit = function (){
-   let currentemail =  form.elements.email.value;
-   let allUsers = JSON.parse(localStorage.getItem('allusers'));
-    allUsers.forEach(function (item,index){
-        if (item.email ===currentemail){
-            alert('User ' + item.name+' існує')
-        }
-    })
-}
+import {getRequestToken, validateWithLogin, getAccess} from './dataservice.js';
+
+let submit = document.getElementById('login');
+submit.onclick = function (){
+   let username =  document.getElementById('username').value;
+   let psw = document.getElementById('pasw').value;
+    getRequestToken(username, psw, validateWithLogin);
+
+   // let allUsers = JSON.parse(localStorage.getItem('allusers'));
+   //  allUsers.forEach(function (item,index){
+   //      if (item.email ===currentemail){
+   //          alert('User ' + item.name+' існує')
+   //      }
+   //  })
+};
+
+// getAccess();
+
